97b6e566d3a3aade62d91446a76dec392db031b4
Savinya Nandakumara <IT20135720@my.sliit.lk>
2021-09-26 12:12:01 +0530

    coded on cart fragment UI

--- app/src/main/java/com/example/flowerparadise/CartFragment.java
+++ app/src/main/java/com/example/flowerparadise/CartFragment.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 //this is the cart fragment
+//Cart fragment connect to navbar
 
 public class CartFragment extends Fragment {
     @Nullable
