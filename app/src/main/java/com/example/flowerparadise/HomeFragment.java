f79637416e886e8e8ff4cb53139d3a29aa0babf7
ChaLan99 <86542778+ChaLan99@users.noreply.github.com>
2021-09-28 21:07:44 +0530

    add image

--- app/src/main/java/com/example/flowerparadise/HomeFragment.java
+++ app/src/main/java/com/example/flowerparadise/HomeFragment.java
@@ -1,18 +1,33 @@
 package com.example.flowerparadise;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 //Updated the home fragment
 public class HomeFragment extends Fragment {
+    private Button btn_home;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable  Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        btn_home = (Button)view.findViewById(R.id.btnhome);
+        btn_home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(getActivity(),HomeActivity.class);
+                startActivity(in);
+            }
+        });
+
+        return view;
     }
 }
