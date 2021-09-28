9088be326715c39c1025a98929bfca96ae58eb97
ChaLan99 <86542778+ChaLan99@users.noreply.github.com>
2021-09-28 20:39:59 +0530

    add cart files

--- app/src/main/java/com/example/flowerparadise/CartFragment.java
+++ app/src/main/java/com/example/flowerparadise/CartFragment.java
@@ -1,19 +1,21 @@
 package com.example.flowerparadise;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-//this is the cart fragment
-//Cart fragment connect to navbar
 
 public class CartFragment extends Fragment {
+
+
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable  Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_cart, container, false);
     }
 }
